47a122de2d51dc7965eeae250dba5ed0e230b60e
Framil <keneraraujo05@outlook.com>
2026-08-03 16:13:51 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,12 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
+  // Força o modo standalone garantido se a variável não vier preenchida
   output: process.env.NEXT_OUTPUT_MODE || 'standalone',
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../'),
-  },
+  
+  // Aponta o tracing para a própria raiz do projeto
+  outputFileTracingRoot: path.join(__dirname),
+  
   eslint: {
     ignoreDuringBuilds: true,
   },
